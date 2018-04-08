5ff3ceb90359cf891ebfc81ac3e04238263cfdaf
Harendra Kumar <harendra.kumar@gmail.com>
2018-04-08 08:38:38 +0530

    Generic config driven presentation of benchmark charts

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,6 +1,125 @@
 module Main where
 
-import Chart (chartMain)
+import Data.Char (isSpace)
+import Data.List.Split (splitOn)
+import Data.Maybe (catMaybes)
+import System.Environment (getArgs)
+import System.Process.Typed (readProcess_)
+import Chart (chartMain, ChartConfig(..), ComparisonStyle (..))
 
+import Data.List
+
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as T
+
+-- XXX use package name and a tag
+packages :: [String]
+packages = ["list", "pure-vector", "vector", "streamly", "streaming", "conduit", "pipes", "machines", "drinkery"]
+
+{-
+-- pairs of benchmark group titles and corresponding list of benchmark
+-- prefixes i.e. without the package name at the end.
+bmGroups :: [(String, [String])]
+bmGroups =
+    [
+      -- Operations are listed in increasing cost order
+      ( "Cheaper Operations (Shorter is Faster)"
+      , [
+          "elimination/toNull"
+        , "filtering/drop-all"
+        , "filtering/dropWhile-true"
+        , "filtering/filter-all-out"
+        , "elimination/last"
+        , "elimination/fold"
+        -- "filtering/take-one"
+        , "transformation/map"
+        , "filtering/take-all"
+        , "filtering/takeWhile-true"
+        , "filtering/filter-all-in"
+        , "filtering/filter-even"
+        , "transformation/scan"
+        ]
+      )
+
+    , ( "Expensive operations (Shorter is Faster)"
+      , [ "transformation/mapM"
+        , "zip"
+        , "transformation/concat"
+        , "elimination/toList"
+        ]
+      )
+    , ( "Composed (4x) operations (Shorter is Faster)"
+      , [ "compose/all-out-filters"
+        , "compose/all-in-filters"
+        , "compose/mapM"
+        , "compose/map-with-all-in-filter"
+        ]
+      )
+    ]
+    -}
+
+-------------------------------------------------------------------------------
 main :: IO ()
-main = chartMain
+main = do
+    args <- getArgs
+
+    (out, _) <- readProcess_ "stack --system-ghc list-dependencies --bench"
+
+    -- Get our streaming packages and their versions
+    let match [] = Nothing
+        match (_ : []) = Nothing
+        match (x : y : _) =
+            case elem x packages of
+                False -> Nothing
+                True -> Just (x, y)
+        -- [(packagename, version)]
+        pkginfo =
+              catMaybes
+            $ map match
+            $ map words (lines (T.unpack $ T.decodeUtf8 out))
+
+    -- suffix versions to packages
+    let suffixVersion p =
+            case lookup p pkginfo of
+                Nothing -> p
+                Just v -> p ++ "-" ++ v
+
+    let titleToFileName t = filter (not . isSpace) (takeWhile (/= '(') t)
+        title = "Cheaper Operations (Shorter is Faster)"
+        prefixes =
+            [ "elimination/toNull"
+            , "filtering/drop-all"
+            , "filtering/dropWhile-true"
+            , "filtering/filter-all-out"
+            , "elimination/last"
+            , "elimination/fold"
+            , "transformation/map"
+            , "filtering/take-all"
+            , "filtering/takeWhile-true"
+            , "filtering/filter-all-in"
+            , "filtering/filter-even"
+            , "transformation/scan"
+            ]
+        cfg = ChartConfig
+            { inputFile = head args
+            , chartTitle = title
+            , outputDir = "charts"
+            , outputFile = Just $ titleToFileName title
+            , classifyBenchmark = \bm ->
+                case any (`isPrefixOf` bm) prefixes of
+                    True ->
+                        let xs = reverse (splitOn "/" bm)
+                        in Just (suffixVersion (xs !! 0), xs !! 1)
+                    False -> Nothing
+            , sortBenchmarks = \bs ->
+                let i = intersect (map (last . splitOn "/") prefixes) bs
+                in i ++ (bs \\ i)
+            , sortBenchGroups = \gs ->
+                let i = intersectBy (\x y -> head (splitOn "-" x) == y)
+                                    gs packages
+                in i ++ (gs \\ i)
+            , selectBgroups = Nothing
+            , timeScale = Nothing
+            , comparisonStyle = CompareFull
+            }
+     in chartMain cfg
--- lib/Chart.hs
+++ lib/Chart.hs
@@ -1,124 +1,54 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Chart (chartMain) where
+module Chart (chartMain, ChartConfig(..), ComparisonStyle (..)) where
 
 import Control.Arrow (second)
-import Data.Char (isSpace)
 import Data.List (nub, transpose)
-import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe, catMaybes, fromMaybe)
 import System.Directory (createDirectoryIfMissing)
-import System.Environment (getArgs)
-import System.Process.Typed (readProcess_)
+-- import System.Process.Typed (readProcess_)
 import Text.CSV (CSV, parseCSVFromFile)
 import Control.Monad.Trans.State.Lazy (get, put)
-
-import qualified Data.Text.Lazy.Encoding as T
-import qualified Data.Text.Lazy as T
 
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams
 
--------------------------------------------------------------------------------
--- Configurable stuff
--------------------------------------------------------------------------------
+data ComparisonStyle = CompareDelta | CompareFull
 
-outputDir :: String
-outputDir = "charts"
+data ChartConfig = ChartConfig
+    {
+      inputFile  :: FilePath
+    , chartTitle  :: String
+    , outputDir   :: FilePath
+    -- | Default is the same as the title
+    , outputFile  :: Maybe FilePath
+    -- | Given a list of translated benchmark names - reorder them for
+    -- presentation in the chart.
+    , sortBenchmarks :: [String] -> [String]
+    , sortBenchGroups :: [String] -> [String]
+    -- | Given a benchmark name return a tuple (groupname, benchname), where
+    -- groupname is the name of the group the benchmark belongs to and the
+    -- benchname is the translated benhcmark name to be displayed on the graph.
+    -- if it returns Nothing the benchmark will be ignored.
+    , classifyBenchmark :: String -> Maybe (String, String)
+    -- | Given the total number of bgroups found in the benchmarked data,
+    -- select the indexes of the ones you want to plot, indexes start from 0.
+    -- default is to plot last 3.
+    , selectBgroups :: Maybe (Int -> [Int])
+    -- | (RangeMax, NumMajorTicks) of the plot on the y (time) axis in microseconds
+    , timeScale      :: Maybe (Double, Int)
+    -- | How to show the comparisons
+    , comparisonStyle :: ComparisonStyle
+    }
 
--- XXX use package name and a tag
-packages :: [String]
-packages = ["list", "pure-vector", "vector", "streamly", "streaming", "conduit", "pipes", "machines", "drinkery"]
-
--- pairs of benchmark group titles and corresponding list of benchmark
--- prefixes i.e. without the package name at the end.
-bmGroups :: [(String, [String])]
-bmGroups =
-    [
-      -- Operations are listed in increasing cost order
-      ( "Cheaper Operations (Shorter is Faster)"
-      , [
-          "elimination/toNull"
-        , "filtering/drop-all"
-        , "filtering/dropWhile-true"
-        , "filtering/filter-all-out"
-        , "elimination/last"
-        , "elimination/fold"
-        -- "filtering/take-one"
-        , "transformation/map"
-        , "filtering/take-all"
-        , "filtering/takeWhile-true"
-        , "filtering/filter-all-in"
-        , "filtering/filter-even"
-        , "transformation/scan"
-        ]
-      )
-
-    , ( "Expensive operations (Shorter is Faster)"
-      , [ "transformation/mapM"
-        , "zip"
-        , "transformation/concat"
-        , "elimination/toList"
-        ]
-      )
-      {-
-    , ( "Discarding and Folding (Shorter is Faster)"
-      , [
-        -- "filtering/take-one"
-          "elimination/toNull"
-        , "filtering/drop-all"
-        , "elimination/last"
-        , "elimination/fold"
-        ]
-      )
-    , ( "Transformation and Filtering (Shorter is Faster)"
-      , [
-          "filtering/filter-all-out"
-        , "filtering/dropWhile-true"
-        , "filtering/take-all"
-        , "filtering/takeWhile-true"
-        , "transformation/map"
-        , "filtering/filter-all-in"
-        , "filtering/filter-even"
-        , "transformation/scan"
-        ]
-      )
-    , ( "Monadic Transformation (Shorter is Faster)"
-      , [
-          "transformation/mapM"
-        ]
-      )
-    , ( "Folding to List (Shorter is Faster)"
-      , [
-          "elimination/toList"
-        ]
-      )
-    , ( "Zipping and Concating Streams (Shorter is Faster)"
-      , [ "zip"
-        , "transformation/concat"
-        ]
-      )
-    -}
-    , ( "Composed (4x) operations (Shorter is Faster)"
-      , [ "compose/all-out-filters"
-        , "compose/all-in-filters"
-        , "compose/mapM"
-        , "compose/map-with-all-in-filter"
-        ]
-      )
-    ]
-
--------------------------------------------------------------------------------
-
--- "values" is [(packageName, [per benchmark result])]
-genGroupGraph :: String -> [String] -> [(String, [Maybe Double])] -> IO ()
-genGroupGraph chartTitle benchNames values =
-    toFile def (outputDir
-                ++ "/"
-                -- links in README.rst eat up the space so we match the same
-                ++ (filter (not . isSpace) (takeWhile (/= '(') chartTitle))
-                ++ ".svg") $ do
+-- "values" is [(benchGroupName, [benchResult])]
+-- benchResult contains results for each benchmark in "benchNames" in exactly
+-- the same order.
+genGroupGraph :: ChartConfig -> [String] -> [(String, [Maybe Double])] -> IO ()
+genGroupGraph ChartConfig{..} benchNames values =
+    -- XXX use filepath/path concatenation
+    toFile def (outputDir ++ "/" ++ fromMaybe chartTitle outputFile ++ ".svg") $ do
         layout_title .= chartTitle
         layout_title_style . font_size .= 25
         layout_x_axis . laxis_generate .= autoIndexAxis (map fst values)
@@ -143,14 +73,12 @@ genGroupGraph chartTitle benchNames values =
             }
         layout_y_axis . laxis_override .= modifyLabels
 
-        {-
-        -- to make comparative charts on a fixed scale
-        layout_y_axis . laxis_override .= \_ ->
-             let indexes = [0,25..125]
-            --let indexes = [0,50..350]
-            --let indexes = [0,100..700]
-            in makeAxis (map ((++ " ms") . show . floor)) (indexes, [], [])
-        -}
+        case timeScale of
+            Nothing -> return ()
+            Just (rangeMax, nticks) ->
+                layout_y_axis . laxis_override .= \_ ->
+                    let indexes = take nticks [0,rangeMax/(fromIntegral nticks)..]
+                    in makeAxis (map ((++ " ms") . show . floor)) (indexes, [], [])
 
         -- XXX We are mapping a missing value to 0, can we label it missing
         -- instead?
@@ -178,85 +106,69 @@ transposeLists xs =
             [] -> replicate n Nothing
             x  -> map Just x
 
--- Given a package name (e.g. streaming), and benchmark prefixes (e.g.
--- [elimination/null, elimination/toList]) get the corresponding results e.g.
--- [8.1 ms, 5.4 ms]. The corresponding result file entries will have
--- elimination/null/streaming etc. as the names of the entries.
---
 -- We return a list of lists as the same benchmark may appear more than once if
 -- we ran benchmarks for the same package multiple times. This is helpful in
 -- comparing the benchmarks for the same package after fixing something.
-getResultsForPackage :: CSV -> String -> [String] -> Maybe [[Maybe Double]]
-getResultsForPackage csvData pkgname bmPrefixes =
-    let bmnames = map (++ "/" ++ pkgname) bmPrefixes
-    in transposeLists $ map getBenchmarkMeans bmnames
+getResultsForBenchGroup
+    :: CSV
+    -> (String -> Maybe (String, String))
+    -> String
+    -> [String]
+    -> Maybe [[Maybe Double]]
+getResultsForBenchGroup csvData classify groupName bmnames  =
+    -- XXX this can be quite inefficient, need to do it better
+    transposeLists $ map getBenchmarkMeans bmnames
 
     where
 
-    -- field at index 1 is the mean
+    match name origName =
+        case classify origName of
+            Nothing -> False
+            Just (g, n) -> g == groupName && n == name
+
     getBenchmarkMeans :: String -> [Double]
     getBenchmarkMeans bmname =
-        map read $ map (!! 1) $ filter ((== bmname) .  head) csvData
+        -- field at index 1 is the mean
+        map read $ map (!! 1) $ filter (match bmname .  head) csvData
 
-genOneGraph :: CSV -> [(String, String)] -> (String, [String]) -> IO ()
-genOneGraph csvData pkginfo (bmGroupTitle, prefixes) =
-    genGroupGraph bmGroupTitle bmTitles bmResults
+genGraph :: ChartConfig -> CSV -> IO ()
+genGraph cfg@ChartConfig{..} csvData =
+    -- bmResults contains benchmark results for bmnames for each group
+    genGroupGraph cfg bmnames bmResults
 
     where
 
-    bmTitles = map (last . splitOn "/" ) prefixes
+    getBenchNames = nub $ map head csvData
+    bmTuples = catMaybes $ map classifyBenchmark getBenchNames
+    -- XXX assert that for each group we get the same bmnames
+    bmnames = sortBenchmarks $ nub $ map snd bmTuples
+    bmgroups = sortBenchGroups $ nub $ map fst bmTuples
 
-    pkgName = fst
-    pkgVersion = snd
-    pkgNameWithVersion pkgInfo = pkgName pkgInfo ++ "-" ++ pkgVersion pkgInfo
-    pkgGetResults pkgInfo =
-        let res = getResultsForPackage csvData (pkgName pkgInfo) prefixes
+    grpGetResults groupName =
+        let res = getResultsForBenchGroup csvData classifyBenchmark
+                                          groupName bmnames
         in case res of
             Nothing -> Nothing
             Just xs ->
                 case length xs of
                     0 -> Nothing
-                    1 -> Just $ map (pkgNameWithVersion pkgInfo,) xs
-                    _ -> Just $ zipWith (withIndexes pkgInfo) [(1::Int)..] xs
-    withIndexes pkgInfo x y =
-        (pkgNameWithVersion pkgInfo ++ "(" ++ show x ++ ")", y)
+                    1 -> Just $ map (groupName,) xs
+                    _ -> Just $ zipWith (withIndexes groupName) [(1::Int)..] xs
+    withIndexes groupName indx y = (groupName ++ "(" ++ show indx ++ ")", y)
 
-    -- this produces results for all packages for all prefixes
-    -- [(packagenamewithversion, [Maybe Double])]
-    bmResults = concat $ catMaybes $ map pkgGetResults pkginfo
-
-genGraphs :: CSV -> [(String, String)] -> IO ()
-genGraphs csvData pkginfo = mapM_ (genOneGraph csvData pkginfo) bmGroups
+    -- this produces results for all groups
+    -- [(groupName, [Maybe Double])]
+    bmResults = concat $ catMaybes $ map grpGetResults bmgroups
 
 -- XXX display GHC version as well
 -- XXX display the OS/arch
--- XXX fix the legend position
-chartMain :: IO ()
-chartMain = do
-    args <- getArgs
+-- This data should be in the measurement data
 
+chartMain :: ChartConfig -> IO ()
+chartMain cfg@ChartConfig{..} = do
     createDirectoryIfMissing True outputDir
 
-    (out, _) <- readProcess_ "stack --system-ghc list-dependencies --bench"
-
-    -- Get our streaming packages and their versions
-    let match [] = Nothing
-        match (_ : []) = Nothing
-        match (x : y : _) =
-            case elem x packages of
-                False -> Nothing
-                True -> Just (x, y)
-        pkginfo =
-              catMaybes
-            $ map match
-            $ map words (lines (T.unpack $ T.decodeUtf8 out))
-
-    -- order them in the order specified in packages so that the order is
-    -- can be controlled by the user.
-    let pkginfo' = map (\x -> (x, fromMaybe "" $ lookup x pkginfo)) packages
-
-    csvData <- parseCSVFromFile (head args)
+    csvData <- parseCSVFromFile inputFile
     case csvData of
         Left e -> error $ show e
-        Right dat -> genGraphs dat pkginfo'
-    return ()
+        Right dat -> genGraph cfg dat
