7076755e7e542deb9d53dcf6942b9d231a778002
Adithya Kumar <adithya@composewell.com>
2022-01-28 00:52:52 +0530

    Add a flag no-colors

--- lib/BenchShow/Internal/Pretty.hs
+++ lib/BenchShow/Internal/Pretty.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -- |
 -- Module      : BenchShow.Internal.Pretty
 -- Copyright   : (c) 2022 Composewell Technologies
@@ -19,4 +21,38 @@ module BenchShow.Internal.Pretty
     , putDoc
     ) where
 
+#ifndef NO_COLORS
+
 import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
+
+#else
+
+type Doc = String
+
+dullred :: Doc -> Doc
+dullred = id
+
+dullgreen :: Doc -> Doc
+dullgreen = id
+
+(<+>) :: Doc -> Doc -> Doc
+(<+>) x y = x ++ " " ++ y
+
+vcat :: [Doc] -> Doc
+vcat = unlines
+
+putDoc :: Doc -> IO ()
+putDoc = putStrLn
+
+fill :: Int -> Doc  -> Doc
+fill i x =
+    let len = length x
+     in x ++ replicate (i - len) ' '
+
+indent :: Int -> Doc -> Doc
+indent i = unlines . map (replicate i ' ' ++) . lines
+
+text :: String -> Doc
+text = id
+
+#endif
